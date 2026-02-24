107ee0e59230771aaa9eb0b5e76b9b3c86989515
AlejandroFV74 <00067323@uca.edu.sv>
2026-02-24 00:21:41 -0600

    fix: update product components and list page

--- my-project/src/app/domains/products/components/product/product.ts
+++ my-project/src/app/domains/products/components/product/product.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-product',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product.html',
   styleUrl: './product.css',
 })
 export class Product {
+  @Input({required: true}) title: string = '';
+  @Input({required: true}) price: number = 0;
+  @Input({required: true}) img: string = '';
+
+  @Output() addToCart = new EventEmitter();
+  
+  addToCartHandler() {
+    console.log('mensaje desde el componente product (hijo)');
+    this.addToCart.emit('este es un mensaje desde el componente ' + this.title);
+  }
 
 }
