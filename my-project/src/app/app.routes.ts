321af1676a4584522b343852897f64041b184455
AlejandroFV74 <00067323@uca.edu.sv>
2026-08-07 21:49:09 -0600

    feat: not found page

--- my-project/src/app/app.routes.ts
+++ my-project/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 
 import { List } from './domains/products/pages/list/list'
+import { NotFound } from './domains/info/pages/not-found/not-found';
 
 export const routes: Routes = [
     {
         path: '',
         component: List
+    },
+    {
+        path: '**',
+        component: NotFound
     }
 ];
