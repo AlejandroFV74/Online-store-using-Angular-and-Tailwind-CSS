35e1eea9e4e212140d091876f0e649416c57031f
AlejandroFV74 <00067323@uca.edu.sv>
2026-08-21 23:02:21 -0600

    feat: product details route added

--- my-project/src/app/app.routes.ts
+++ my-project/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { List } from './domains/products/pages/list/list';
 import { Layout } from './domains/shared/components/layout/layout';
 import { NotFound } from './domains/info/pages/not-found/not-found';
+import {ProductDetail} from './domains/products/pages/product-detail/product-detail';
 
 export const routes: Routes = [
   {
@@ -12,6 +13,10 @@ export const routes: Routes = [
       {
         path: '',
         component: List,
+      },
+      {
+        path: 'product/:id',
+        component: ProductDetail,
       }
     ],
   },
--- my-project/src/app/domains/products/components/product/product.ts
+++ my-project/src/app/domains/products/components/product/product.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '../../../shared/models/product';
 import { TimeAgoPipe } from '../../../shared/pipes/time-ago-pipe';
+import { RouterLinkWithHref } from "@angular/router";
 
 @Component({
   selector: 'app-product',
-  imports: [CommonModule, TimeAgoPipe],
+  imports: [CommonModule, TimeAgoPipe, RouterLinkWithHref],
   templateUrl: './product.html',
   styleUrl: './product.css',
 })
